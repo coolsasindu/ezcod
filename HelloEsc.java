5614c402e0c7d5f3e1223273b348551c21316ead
Sasindu umesh <45946252+coolsasindu@users.noreply.github.com>
2021-02-24 14:51:47 +0530

    Add files via upload

---
+++ HelloEsc.java
@@ -0,0 +1,11 @@
+class HelloEsc {
+  
+ public static void main(String args[]){
+   
+    System.out.println("Hello World This is\bezcod.blogspot.com "); // \b = backspace
+    System.out.println("Hello World This is\f ezcod.blogspot.com "); // \f  = Form feed (*)
+    System.out.println("Hello World This is\n ezcod.blogspot.com ");  // \n = Print New line
+    System.out.println("Hello World\r This is ezcod.blog\tspot.com ");  // \r = Carriage return ( Enter) \t = tab (TAB Space)
+    
+ }
+}
