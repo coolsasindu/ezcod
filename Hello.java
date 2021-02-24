c5572b9aeb54acfdc994662529418fa1a64c104c
Sasindu umesh <45946252+coolsasindu@users.noreply.github.com>
2021-02-24 13:43:30 +0530

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 class Hello {
+  
  public static void main(String args[]){
-    System.out.println("Hello World This is\b ezcod.blogspot.com ");
-    System.out.println("Hello World This is\f ezcod.blogspot.com ");
-    System.out.println("Hello World This is\n ezcod.blogspot.com ");
-    System.out.println("Hello World\r This is ezcod.blog\tspot.com ");	
+   
+     System.out.println("Hello World This is ezcod.blogspot.com ");
+     
  }
 }
