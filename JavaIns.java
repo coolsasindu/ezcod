ae2fc2a3dbcd1e81f3f374d003bc8288a69a80c3
Sasindu umesh <45946252+coolsasindu@users.noreply.github.com>
2021-02-24 16:36:00 +0530

    Add files via upload

---
+++ JavaIns.java
@@ -0,0 +1,17 @@
+
+public class JavaIns {
+ 
+    public static void main(String[] args) {
+   
+      String Name; //  Declaring  Variables 
+      
+      Name = "my name is"; //  Initializing Value  
+      
+     String Java = "java 8.1"; //  Declaring and Initializing Variables    
+    
+         System.out.println(Name);
+         System.out.println(Java); 
+      
+    }
+    
+}
