45c3d558bd13e987c49cdb8927249a81978d7a6b
Sasindu umesh <45946252+coolsasindu@users.noreply.github.com>
2021-02-24 15:36:29 +0530

    Add files via upload

---
+++ JavaData.java
@@ -0,0 +1,44 @@
+
+public class JavaData {
+ 
+    public static void main(String[] args) {
+   
+      String Name; //  Declaring  Variables      
+      Name = "my name is"; //  Initializing Value     
+     String java = "java 8.1"; //  Declaring and Initializing Variables    
+       
+     byte b = 127 ; 
+     short s = 32767;
+     int  i = 2147483645 ; 
+     long  l = 9223372036854775807L;
+     
+     char c = 'D';
+     String str = "Character ABCDKKSJKJS and number 6454654 Any !@#$%^&*()_+" ;
+     
+    
+     float f  = 12.12f ;
+     double d = 3232.323232 ;
+     
+     boolean bt = true;
+     boolean bf = false; 
+        
+     System.out.println(Name);
+     System.out.println(java);
+     System.out.println(b);
+     System.out.println(s);
+     System.out.println(i);
+     System.out.println(l);
+     System.out.println(c);
+     System.out.println(str);
+     System.out.println(f);
+     System.out.println(d);
+     System.out.println(bt);
+     System.out.println(bf);
+     
+     
+    
+     
+     
+    }
+    
+}
