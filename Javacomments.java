5c381aca14e97f555157b5c8b61c58ed1891e8cd
Sasindu umesh <45946252+coolsasindu@users.noreply.github.com>
2021-02-24 15:15:37 +0530

    Add files via upload

---
+++ Javacomments.java
@@ -0,0 +1,24 @@
+class Javacomments {
+  
+ public static void main(String args[]){   
+    System.out.println("Hello Check this line Ending  "); // i am Single line comment
+   
+     /* 
+      i am multiple-line comment 
+      can span 
+      several lines 
+      
+      */
+    System.out.println("i am multiple-line comment ");  
+    
+    
+    /**
+ *  This is Document Comments .
+ *  @author ezcod.blogspot.com.
+ *  @version 4.5
+ */
+    
+    System.out.println("Documentation Comment"); 
+    
+ }
+}
