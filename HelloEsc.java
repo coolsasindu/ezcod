aed181db01b76fa0f05f38a408ab07bc25d8a645
Sasindu umesh <45946252+coolsasindu@users.noreply.github.com>
2021-02-24 15:19:23 +0530

    Update HelloEsc.java

--- HelloEsc.java
+++ HelloEsc.java
@@ -2,6 +2,21 @@ class HelloEsc {
   
  public static void main(String args[]){
    
+   /*
+       Escape Characters
+        
+       \b = big
+       \f = farm
+       \n = need
+       \r = red
+       \t = tractor        
+       \"
+       \'
+       \\
+      
+        
+      */
+   
     System.out.println("Hello World This is\bezcod.blogspot.com "); // \b = backspace
     System.out.println("Hello World This is\f ezcod.blogspot.com "); // \f  = Form feed (*)
     System.out.println("Hello World This is\n ezcod.blogspot.com ");  // \n = Print New line
